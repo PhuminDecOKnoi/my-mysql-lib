a20a7c2e7eaa3a01408bac825c76aebe8cbb0960
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-17 00:01:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
     if(!$conn) die("Disconnected with MySQL");
 
     $ADODB_FETCH_MODE = ADODB_FETCH_NUM;
-    $ADODB_COUNTRECS = TRUE;
-
     $get_users = select_tb_user($conn);
-    $num_rows_user = count_rows_user($conn);
     $get_tests  = select_tb_test($conn);
+
+    $ADODB_COUNTRECS = TRUE;
+    $num_rows_user = count_rows_user($conn);
     $num_rows_test = count_rows_test($conn);
 
     include_once("view_index.php");
