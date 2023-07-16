9892fcaab12f65bdafc8863d48592e7a64f3ad38
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-16 22:33:53 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+    include("./functions.php");
+
+    $conn = conn();
+    if(!$conn) die("Disconnected with MySQL");
+
+    $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+
+    $sql = 'SELECT * FROM `tb_user`';
+    $result = $conn->getAll($sql);
+
+    print_r($result);
+?>
