ebe325c475f9d2df85ed6dbac4ffad4cbb63bf7a
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-16 23:50:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+    include("./functions.php");
+
+    $sql = "";
+    $get_user = array();
+    $get_test = array();
+
+    $conn = conn();
+    if(!$conn) die("Disconnected with MySQL");
+
+    $ADODB_FETCH_MODE = ADODB_FETCH_NUM;
+    $ADODB_COUNTRECS = TRUE;
+
+    $get_users = select_tb_user($conn);
+    $num_rows_user = count_rows_user($conn);
+    $get_tests  = select_tb_test($conn);
+    $num_rows_test = count_rows_test($conn);
+
+    include_once("view_index.php");
+?>
