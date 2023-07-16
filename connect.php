74e3b74324b7223e2fe832b5f546db55d085b5d1
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-16 22:11:44 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,6 @@
     require_once('./adodb/adodb.inc.php'); //C:\xampp\htdocs\my-mysql-lib\adodb\adodb.inc.php
 
     $driver = 'mysqli';
-
     $localost = "localhost";
     $user = "root";
     $password = "";
@@ -12,12 +11,11 @@
     $db = adoNewConnection($driver);
     $db->connect($localost,$user,$password,$database);
 
-    if(!$db) echo "errer!";
-
-    $sql = 'SELECT * FROM `tb_user`';
+    if(!$db) die("Disconnected!");
 
     $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
-    //$result = $db->Execute($sql);
+
+    $sql = 'SELECT * FROM `tb_user`';
     $result = $db->getAll($sql);
 
     print_r($result);
