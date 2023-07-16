3d831a659acd41ede846efc044aa37fe54675b1b
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-16 22:46:29 +0700

    Create mysql.php

---
+++ mysql.php
@@ -0,0 +1,24 @@
+<?php
+    
+    require_once('./adodb/adodb.inc.php'); //C:\xampp\htdocs\my-mysql-lib\adodb\adodb.inc.php
+
+    $driver = "mysqli";
+    $localost = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "test";
+    $sql = "";
+    $result = array();
+
+    $db = adoNewConnection($driver);
+    $db->connect($localost,$user,$password,$database);
+
+    if(!$db) die("Disconnected!");
+
+    $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+
+    $sql = 'SELECT * FROM `tb_user`';
+    $result = $db->getAll($sql);
+
+    print_r($result);
+?>
