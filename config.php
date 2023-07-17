5ee7be178b138a1a8eeef8acc22601ff75800a24
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-17 21:55:24 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+    require_once('./adodb/adodb.inc.php'); //C:\xampp\htdocs\my-mysql-lib\adodb\adodb.inc.php
+    
+    define("DRIVER","mysqli");
+    define("LOCALHOST","localhost");
+    define("USER","root");
+    define("PWD","");
+    define("DB","test");
+
+    define("TB_USERS","`tb_user`");
+    define("TB_TESTS","`tb_test`");
+
+?>
