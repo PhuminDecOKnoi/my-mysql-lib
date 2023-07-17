f1c3b6b8ae2c2d1132663966ec118dc08f2852ee
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-17 21:55:28 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,40 +1,38 @@
 <?php
-    require_once('./adodb/adodb.inc.php'); //C:\xampp\htdocs\my-mysql-lib\adodb\adodb.inc.php
-    
-    define("DRIVER","mysqli");
-    define("LOCALHOST","localhost");
-    define("USER","root");
-    define("PWD","");
-    define("DB","test");
-    
+
+    include_once('./config.php');
+
     function conn(){
         $db = adoNewConnection(DRIVER);
         $db->connect(LOCALHOST,USER,PWD,DB);
         return $db;
     }
     function select_tb_user($con){
-        $sql = 'SELECT * FROM `tb_user`';
+        $sql = 'SELECT * FROM '.TB_USERS;
         $result = $con->GetRow($sql);
         return $result;
     }
     function select_tb_test($con){
-        $sql = 'SELECT * FROM `tb_test`';
+        $sql = 'SELECT * FROM '.TB_TESTS;
         $result = $con->GetRow($sql);
         return $result;
     }
     function count_rows_user($con){
-        $sql = 'SELECT * FROM `tb_user`';
+        $sql = 'SELECT * FROM '.TB_USERS;
         $result = $con->Execute($sql);
         $num_row_user = $result->recordCount();
         return (int) $num_row_user;
     }
     function count_rows_test($con){
-        $sql = 'SELECT * FROM `tb_test`';
+        $sql = 'SELECT * FROM '.TB_TESTS;
         $result = $con->Execute($sql);
         $num_row_user = $result->recordCount();
         return (int) $num_row_user;
     }
     function title_index_page(){
         echo "index page";
+    }
+    function title_register_page(){
+        echo "register page";
     }
 ?>
