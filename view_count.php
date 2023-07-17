978b2a9458d765c731fa9bb99d14c90f0166453b
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-17 22:25:11 +0700

    Create view_count.php

---
+++ view_count.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php title_count_page(); ?></title>
+    </head>
+<body>
+    <label>User: </label><?= $num_rows_user ?>
+    <hr/>
+    <label>Test: </label><?= $num_rows_test ?>
+</body>
+</html>
