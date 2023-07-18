835b93ed58011ed86e680ca3f28d0192fbdf9bb2
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-19 00:47:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
-
+    // file Controller.
     include("./functions.php");
 
     $sql = "";
     $get_user = array();
     $get_test = array();
 
-    $conn = conn();
+    $conn = conn(); // Connected with mySQL.
     if(!$conn) die("Disconnected with MySQL");
 
-    $ADODB_FETCH_MODE = ADODB_FETCH_NUM;
+    $ADODB_FETCH_MODE = ADODB_FETCH_NUM; //return query like rows.
     $get_users = select_tb_user($conn);
     $get_tests  = select_tb_test($conn);
 
-    include_once("view_index.php");
+    include_once("view_index.php"); //display index => HTML file.
 ?>
