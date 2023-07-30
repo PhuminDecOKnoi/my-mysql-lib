7ed1683ac88262b74d9f5412bec0553483d44b0b
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-30 11:30:04 +0700

    Create view_delete.php

---
+++ view_delete.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <title></title>
+    </head>
+    <body>
+        <?php echo $message; ?>
+        <div>
+            <a href="index.php">Home</a>
+        </div>
+    </body>
+</html>
