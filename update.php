6e840b8dd034e5c20979bb80781577be0649e602
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-30 11:29:59 +0700

    Update update.php

--- update.php
+++ update.php
@@ -2,23 +2,27 @@
    // file Controller.
    include("./functions.php");
 
+   $get = $_GET['id'];
    $query = false;
+   $message = "";
    $record = array();
    $update = "";
 
    $conn = conn(); // Connected with mySQL.
-   if(!$conn) die("Disconnected with MySQL");
+   if(!$conn) $message = "Disconnected with MySQL";
   
-  
-   $record["user"] = "phumin_ak0099993333000_222";
-   $record["Password"]  = "12345_ak"; 
-   $update = 'ID=11';
- 
-   $query = $conn->autoExecute(TB_TESTS,$record,'UPDATE',$update);
-   
-   if($query){
-      echo "Updated !";
-   }
-   
-   // include_once("view_index.php"); //display index => HTML file.
+    // Update by record array
+   $record["user"] = "phumin_ak00999944444000555555555_222";
+   $record["Password"]  = "12345XXXXXXX_ak"; 
+
+   if(isset($get)){
+        $where = 'ID='.$get;
+        $query = updated_record($conn,$record,$where);
+        if($query) $message = "Updated = ".$get;
+
+   }else{
+        $message = "id not isset !";
+    }
+
+   include_once("view_update.php"); //display index => HTML file.
 ?>
