2cc5c980ba512c58c56af273782d0f4c4a195bba
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-30 11:29:54 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -4,24 +4,18 @@
 
    // $sql = "";
    $query = false;
-
+   $message = "";
+   
    $conn = conn(); // Connected with mySQL.
-   if(!$conn) die("Disconnected with MySQL");
-
-    /*
-   $sql = "INSERT INTO tb_test (user ,Password) VALUES('phumin_ak','12345')";
-   $query = $conn->Query($sql);
-   */
+   if(!$conn) $message = "Disconnected with MySQL";
   
    $record = array();
    $record["user"] = "phumin_ak000000_111";
    $record["Password"]  = "12345_ak"; 
- 
-   $query = $conn->autoExecute(TB_TESTS,$record,'INSERT');
    
-   if($query){
-      echo "Inserted !";
-   }
-   
-   // include_once("view_index.php"); //display index => HTML file.
+   $query = inserted_record($conn,$record);
+
+   if($query) $message = "Inserted !";
+
+   include_once("view_insert.php"); //display index => HTML file.
 ?>
