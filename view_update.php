25bdbba4d7484f421236f4fe3aa8bcc0a5988e36
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-30 11:30:12 +0700

    Create view_update.php

---
+++ view_update.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <?php echo $message; ?>
+        <div>
+            <a href="index.php">Home</a>
+        </div>
+    </body>
+</html>
