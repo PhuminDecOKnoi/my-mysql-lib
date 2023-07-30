376a6460baeb35ef6c79873a9a7ebec8c5706a2b
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-31 00:11:29 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,8 @@
 <?php
     require_once('./adodb/adodb.inc.php'); //C:\xampp\htdocs\my-mysql-lib\adodb\adodb.inc.php
+
+    define('SMARTY_DIR', 'C:/xampp/htdocs/my-mysql-lib/smarty/libs/'); // set path to Smarty directory *nix style
+    require_once(SMARTY_DIR . 'Smarty.class.php');
     
     define("DRIVER","mysqli");
     define("LOCALHOST","localhost");
@@ -8,5 +11,5 @@
     define("DB","test");
 
     define("TB_USERS","`tb_user`");
-    define("TB_TESTS","`tb_test`");
+    define("TB_TESTS","`tb_test`"); 
 ?>
