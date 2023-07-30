06054427abb9b209f5b74f3ec7118e1982d7f814
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-31 00:11:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,5 +15,20 @@
     $sql ="SELECT * FROM `tb_test`";
     $query = $conn->execute($sql);
 
-    include_once("view_index.php"); //display index => HTML file.
+    // FetchRow
+    $arr = array();
+    $i =0;
+    while($result = $query->fetchrow()){
+        $arr[$i] = $result;
+        $i++;
+    }
+
+    // include_once("view_index.php"); //display index => HTML file.
+    $smarty = smarty();
+
+    $smarty->assign('title','Index page');
+    $smarty->assign('message',$message);
+    $smarty->assign('arr',$arr); //sql->excute->fetchrow
+
+    $smarty->display('index.tpl');
 ?>
