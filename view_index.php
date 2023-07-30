ef034006a8aa973d3adb3e3b8c7ca873378bec3a
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-30 11:30:07 +0700

    Update view_index.php

--- view_index.php
+++ view_index.php
@@ -7,19 +7,18 @@
     <!-- Start Page-->
     <h1>Adodb Query</h1>
     <hr/>
-    <?= print_r($get_users); ?>
-    <hr/>
-    <?= print_r($get_tests);?>
     <div>
-        <div><?= $get_users[1]?></div>
-        <div><?= $get_users[2]?></div>
-        <div><?= $get_users[3]?></div>
+        <a href="insert.php">Add record</a>
+        <a href="update.php?id=8">Update record</a>
+        <a href="delete.php?id=12">Delete record</a>
     </div>
     <div>
-        <div><?= $get_tests[1]?></div>
-        <div><?= $get_tests[2]?></div>
+    <?php
+        while($result = $query->fetchrow()){ ?>
+            <div><?php echo $result[1].$result[2] ?></div>
+            <div>##########$$$$$$$$$$</div>
+    <?php } ?>
     </div>
-    <div></div>
     <!-- End Page-->
 </body>
 </html>
